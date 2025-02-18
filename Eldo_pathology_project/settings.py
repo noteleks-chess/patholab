2efa8c1f9d522f315100a623d646ca7700b99011
Chesori <chessbethwell@gmail.com>
2025-02-18 22:07:57 +0300

    Restructure commit

--- Eldo_pathology_project/settings.py
+++ Eldo_pathology_project/settings.py
@@ -78,19 +78,22 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Eldo_pathology_project.wsgi.application'
+WSGI_APPLICATION = 'Eldo_pathology_project.wsgi:application'
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': os.environ.get('DATABASE_NAME'),
+         'USER': os.environ.get('DATABASE_USER'),
+         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+         'HOST': os.environ.get('DATABASE_HOST'),
+         'PORT': os.environ.get('DATABASE_PORT'),
+     }
+ }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
