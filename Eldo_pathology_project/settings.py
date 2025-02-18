92decc7997d9793783cf754034fe8dd05723fb02
Chesori <chessbethwell@gmail.com>
2025-02-18 21:56:07 +0300

    Initial commit of Eldolab

--- Eldo_pathology_project/settings.py
+++ Eldo_pathology_project/settings.py
@@ -84,16 +84,13 @@ WSGI_APPLICATION = 'Eldo_pathology_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': os.environ.get('DATABASE_NAME'),
-         'USER': os.environ.get('DATABASE_USER'),
-         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-         'HOST': os.environ.get('DATABASE_HOST'),
-         'PORT': os.environ.get('DATABASE_PORT'),
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
