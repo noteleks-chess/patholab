a9f47b4ed6be215b405efb904f41802b29c96912
Chesori <chessbethwell@gmail.com>
2025-02-21 11:55:53 +0300

    admin field changes

--- Eldo_pathology_project/settings.py
+++ Eldo_pathology_project/settings.py
@@ -83,7 +83,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Eldo_pathology_project.wsgi:application'
 
-
+print("DATABASE_URL:", os.environ.get('DATABASE_URL'))
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
